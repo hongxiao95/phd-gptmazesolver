3dd186f81acb916f267fdfb4b10372ad7015bb96
hongxiao95 <hongxiao95@hotmail.com>
2023-05-15 07:17:46 +0900

    fi bugs

--- mazesolver.py
+++ mazesolver.py
@@ -1,7 +1,7 @@
 from maze_generator import gen_maze
 import openai
 import os
-import copy
+import json
 
 
 DIR_UP = "up"
@@ -11,7 +11,25 @@ DIR_RIGHT = "right"
 
 def main():
     exit_mark = "Q"
-    ent, exit, maze = gen_maze(10, 10, surrand=True, exit_mark="Q")
+    ent_mark = "O"
+    map_file_name = input("input map file name in [maps], leave blank and gen new map: ")
+    ent, exit, maze = (0,0,0)
+    using_prev_tip = True if input("Using Pre-Step-Tips to improve efficent?(y/n)").strip().lower() == "y" else False
+    if map_file_name.strip() == "" or os.path.exists("maps" + os.path.sep + map_file_name) == False:
+        ent, exit, maze = gen_maze(20, 20, surrand=True, exit_mark="Q", entrance_mark="O")
+        new_map_file_name = get_new_map_file_name()
+        if new_map_file_name == False:
+            print("Too many map files, not saved")
+        else:
+            with open(new_map_file_name, "w") as new_map_file:
+                for line in maze:
+                    new_map_file.write("".join(line) + "\n")
+            print("New Map Saved as " + new_map_file_name)
+    else:
+        map_file_name = "maps" + os.path.sep + map_file_name
+        print("reading from " + map_file_name)
+        ent, exit, maze = read_map(map_file_name, ent_mark, exit_mark)
+        input("Reading Finished")
     api_key = None
     if os.path.exists("proj_config/api_key.conf") == False:
         print("No API-key File!")
@@ -24,12 +42,12 @@ def main():
             return
     
     openai.api_key = api_key
-    print([x["id"] if "gpt" in x["id"] else 0 for x in openai.Model.list()["data"]])
+    # print([x["id"] if "gpt" in x["id"] else 0 for x in openai.Model.list()["data"]])
 
     for line in maze:
         print(''.join(line))
 
-    solve_by_gpt_4(ent, maze, "Q")
+    solve_by_gpt_4(ent, maze, "Q", using_prev_tip=using_prev_tip)
     
     print(ent)
     print(exit)
@@ -49,7 +67,7 @@ def get_maze_str(maze, current_pos, history_indexes):
             cache_str += item
         cache_str += "\n"
     return cache_str
-def get_four_closet(maze, pos, exit_mark, natural=False):
+def get_four_closet(maze, pos, exit_mark, natural=False, last_step_dir = ""):
 
     # 上 左 下 右
     ori = [maze[pos[0] - 1][pos[1]], maze[pos[0]][pos[1] - 1], maze[pos[0] + 1][pos[1]], maze[pos[0]][pos[1] + 1]]
@@ -59,7 +77,7 @@ def get_four_closet(maze, pos, exit_mark, natural=False):
     res = "-".join(li)
 
     if natural:
-        res = f"at this position, the up side is {li_nat[0]}, the left side is {li_nat[1]}, the down side is {li_nat[2]} and the right side is {li_nat[3]}"
+        res = f"at this position, the up side is {li_nat[0]} {'(you came from there)' if last_step_dir == DIR_DOWN else ''}, the left side is {li_nat[1]}{'(you came from there)' if last_step_dir == DIR_RIGHT else ''}, the down side is {li_nat[2]}{'(you came from there)' if last_step_dir == DIR_UP else ''} and the right side is {li_nat[3]}{'(you came from there)' if last_step_dir == DIR_LEFT else ''}"
     return res
 
 def one_step_move(pos, dir):
@@ -72,7 +90,7 @@ def one_step_move(pos, dir):
     if dir == DIR_RIGHT:
         return (pos[0], pos[1] + 1)
 
-def solve_by_gpt_4(ent, maze, exit_mark):
+def solve_by_gpt_4(ent, maze, exit_mark, using_prev_tip):
     SYSTEM_NAME = "system"
     USER_NAME = "user"
     ASSISTANT_NAME = "assistant"
@@ -109,10 +127,16 @@ def solve_by_gpt_4(ent, maze, exit_mark):
     history_dirs = []
 
     while finish == False:
-        resp_msg = talk_gpt_4(messages=messages)
+        resp_msg = ""
+        try:
+            resp_msg = talk_gpt_4(messages=messages, use_4 = True)
+        except Exception as e:
+            print(f"err of {e}")
+            continue
         instruction = resp_msg[PARAM_CONTENT].lower()
 
         if instruction not in valid_cmd:
+            print("instrucaion invalid: " + instruction)
             if retry_times > 5:
                 print("Too many retry")
                 break
@@ -122,7 +146,7 @@ def solve_by_gpt_4(ent, maze, exit_mark):
             retry_times = 0
 
         new_pos = one_step_move(current_pos, instruction)
-        if maze[new_pos[0]][new_pos[1]] != " ":
+        if maze[new_pos[0]][new_pos[1]] != " " and maze[new_pos[0]][new_pos[1]] != exit_mark:
             if bad_pos_retry > 5:
                 print("Too many retry")
                 break
@@ -145,21 +169,50 @@ def solve_by_gpt_4(ent, maze, exit_mark):
             finish = True
             continue  
         else:
-            four_closest = get_four_closet(maze, current_pos, exit_mark=exit_mark, natural=use_natural_feedback)
+            four_closest = get_four_closet(maze, current_pos, exit_mark=exit_mark, natural=use_natural_feedback, last_step_dir= instruction if using_prev_tip else "")
             
-            messages = [messages[0]] + [{PARAM_ROLE:USER_NAME, PARAM_CONTENT:f"From the entrance, you've already walked the following step:[{','.join(history_dirs)}] and now " + four_closest + "which direction will you go next?"}]
-            print(messages[1])
+            messages.append(resp_msg)
+            messages.append({PARAM_ROLE:USER_NAME, PARAM_CONTENT:four_closest})
+            print(messages[-1])
+            with open("logs/testlog.log", "w") as logfile:
+                logfile.write(maze_str)
+                logfile.write(json.dumps(messages))
             # input("press to next")
 
-def talk_gpt_4(messages):
+def talk_gpt_4(messages, use_4 = True):
     completion = openai.ChatCompletion.create(
-        model="gpt-4-0314",
+        model="gpt-4-0314" if use_4 else "gpt-3.5-turbo-0301",
         messages=messages,
         n = 1,
         temperature = 1,
         stream = False
     )
     return completion.choices[0]["message"]
+
+def get_new_map_file_name():
+    for i in range(100):
+        file_name = "maps" + os.path.sep + "map" + str(i) + ".txt"
+        if os.path.exists(file_name) == False:
+            return file_name
+    return False
+
+def read_map(filename, ent_mark, exit_mark):
+    with open(filename) as map_file:
+        lines = map_file.readlines()
+        map = []
+        for line in lines:
+            map.append(list(line.strip()))
+        ent = []
+        exit = []
+        for y in range(len(map)):
+            for x in range(len(map[y])):
+                if map[y][x] == ent_mark:
+                    ent = (y,x)
+                if map[y][x] == exit_mark:
+                    exit = (y,x)
+
+        return (ent, exit, map)
+
 
 if __name__ == "__main__":
     main()
